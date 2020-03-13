efb509d5432db2ebc2eabdc5cd0031573de47b0d
orzzzc <62094847+orzzzc@users.noreply.github.com>
2020-03-13 17:01:43 +0800

    Add files via upload

--- orzzzc.js
+++ orzzzc.js
@@ -24,7 +24,7 @@ function orzorz() {
 }
 var obj=new Date();
 var orzCount=0,orzDelta=1,dianji=0,changedianji=1,cjresttime=0,lastcj=0;
-var flag=new Array(1,1,1,1,1,1,1,1,1);
+var flag=new Array(1,1,1,1,1,1,1,1,1,1);
 var sayslist=new Array("我太强了","我txdy","你们怎么这么菜啊","这些黑题都是些sb题，我随便切","IOI太简单了，我都不屑于AK","我吊打你们","我是最强的","我无敌了","我rank1稳了");
 function ORZINF(){
     obj=new Date();
@@ -43,11 +43,11 @@ function ORZINF(){
     if(orzCount>=100000)if(flag[5]==1)flag[5]=0,orzDelta*=1.25,document.getElementById("orzbutton").innerHTML="膜拜zzc "+orzDelta+" 次";
     if(orzCount>=200000)if(flag[6]==1)flag[6]=0,orzDelta*=2,document.getElementById("orzbutton").innerHTML="膜拜zzc "+orzDelta+" 次";
     if(orzCount>=500000)if(flag[7]==1)flag[7]=0,orzDelta*=2.5,document.getElementById("orzbutton").innerHTML="膜拜zzc "+orzDelta+" 次";
-    if(orzCount>=1000000)if(flag[8]=1)flag[8]=0,orzDelta*=3,document.getElementById("orzbutton").innerHTML="膜拜zzc "+orzDelta+" 次";
+    if(orzCount>=1000000)if(flag[8]==1)flag[8]=0,orzDelta*=3,document.getElementById("orzbutton").innerHTML="膜拜zzc "+orzDelta+" 次";
     if(orzCount>=100000000000000000000) {
         document.getElementById("mainarea").innerHTML="<h1 style=\"color:red\" align=\"center\">zzc:你这个菜鸡别膜我了，反正也不可能变得像我这么强<br>我还要去THU吊打集训队，还要去MIT秒题<br>（zzc说着走起了路准备离开）</h1><br><br><div id=\"walkzzc\" align=\"center\"></div><br><button style=\"font-size: 24px\" onclick=\"last_orz()\" id=\"lastorz\">继续膜拜...</button>";
     }
-    if(dianji==1000)document.getElementById("zzcsays2").innerHTML='获得成就 狂热：点击膜拜按钮1000次<br>单次点击膜拜数翻倍',orzDelta*=2,document.getElementById("orzbutton").innerHTML="膜拜zzc "+orzDelta+" 次",cjresttime=1,lastcj=obj.getHours()*3600+obj.getMinutes()*60+obj.getSeconds();
+    if(dianji>=1000)if(flag[9]==1)flag[9]=0,document.getElementById("zzcsays2").innerHTML='获得成就 狂热：点击膜拜按钮1000次<br>单次点击膜拜数翻倍',orzDelta*=2,document.getElementById("orzbutton").innerHTML="膜拜zzc "+orzDelta+" 次",cjresttime=1,lastcj=obj.getHours()*3600+obj.getMinutes()*60+obj.getSeconds();
 }
 
 function last_orz() {
