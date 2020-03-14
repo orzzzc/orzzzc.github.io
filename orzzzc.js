e1f53012e85ced42ae66e0efa5b9534d6910df82
orzzzc <62094847+orzzzc@users.noreply.github.com>
2020-03-14 19:22:34 +0800

    Add files via upload

--- orzzzc.js
+++ orzzzc.js
@@ -25,13 +25,13 @@ function orzorz() {
 var obj=new Date();
 var orzCount=0,orzDelta=1,dianji=0,changedianji=1,cjresttime=0,lastcj=0,lastmb=0,tttt=0;
 var flag=new Array(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1);
-var sayslist=new Array("我太强了","我txdy","你们怎么这么菜啊","这些黑题都是些sb题，我随便切","IOI太简单了，我都不屑于AK","我吊打你们","我是最强的","我无敌了","我rank1稳了");
+var sayslist=new Array("我太强了","我txdy","你们怎么这么菜啊","这些黑题都是些sb题，我随便切","IOI太简单了，我都不屑于AK","我吊打你们","我是最强的","我无敌了","我rank1稳了","ZJOI都是送分题","IMO好多sb题","多项式太简单了","我是神仙","你们怎么学OI的，这么简单的题都不会","我又发明了一种算法","我怎么这么强","这10k+的模拟我用记事本写一遍过","CF3800+太简单了");
 function ORZINF(){
     obj=new Date();
     orzCount+=orzDelta;
     dianji+=1;
     flag[0]=1;
-    if(cjresttime==1)if(obj.getHours()*3600+obj.getMinutes()*60+obj.getSeconds()-lastcj>=5)cjresttime=0;
+    if(cjresttime==1)if(obj.getHours()*3600+obj.getMinutes()*60+obj.getSeconds()-lastcj>=8)cjresttime=0;
     if(cjresttime==0)document.getElementById("zzcsays2").innerHTML="";
     if(orzCount<10000)document.getElementById("orzTimes").innerHTML='已膜拜zzc '+orzCount+' 次';
     else if(orzCount<10000000)document.getElementById("orzTimes").innerHTML='已膜拜zzc '+Math.floor(orzCount/1000)+'.'+Math.floor(orzCount/100)%10+'k 次';
@@ -51,12 +51,19 @@ function ORZINF(){
     if(orzCount>=20000000)if(flag[16]==1)flag[16]=0,orzDelta/=3,orzDelta*=5,flag[0]=0;
     if(orzCount>=50000000)if(flag[17]==1)flag[17]=0,orzDelta*=1.8,flag[0]=0;
     if(orzCount>=150000000)if(flag[18]==1)flag[18]=0,orzDelta/=3,orzDelta*=5,flag[0]=0;
+    if(orzCount>=450000000)if(flag[19]==1)flag[19]=0,orzDelta/=3,orzDelta*=5,flag[0]=0;
+    if(orzCount>=1000000000)if(flag[20]==1)flag[20]=0,orzDelta*=1.5,flag[0]=0;
+    if(orzCount>=2500000000)if(flag[21]==1)flag[21]=0,orzDelta*=2,flag[0]=0;
+    if(orzCount>=6000000000)if(flag[22]==1)flag[22]=0,orzDelta*=2.5,flag[0]=0;
+    if(orzCount>=20000000000)if(flag[23]==1)flag[23]=0,orzDelta*=1.2,flag[0]=0;
+    if(orzCount>=50000000000)if(flag[24]==1)flag[24]=0,orzDelta/=3,orzDelta*=5,flag[0]=0;
+    if(orzCount>=120000000000)if(flag[25]==1)flag[25]=0,orzDelta*=1.2,flag[0]=0;
     //if(orzCount>=100000000000000000000) {
     //    document.getElementById("mainarea").innerHTML="<h1 style=\"color:red\" align=\"center\">zzc:你这个菜鸡别膜我了，反正也不可能变得像我这么强<br>我还要去THU吊打集训队，还要去MIT秒题<br>（zzc说着走起了路准备离开）</h1><br><br><div id=\"walkzzc\" align=\"center\"></div><br><button style=\"font-size: 24px\" onclick=\"last_orz()\" id=\"lastorz\">继续膜拜...</button>";
     //}
     if(dianji>=500)if(flag[9]==1)flag[9]=0,document.getElementById("zzcsays2").innerHTML='获得成就 狂热LV.1：点击膜拜按钮500次<br>单次点击膜拜数翻倍',orzDelta*=2,flag[0]=0,cjresttime=1,lastcj=obj.getHours()*3600+obj.getMinutes()*60+obj.getSeconds();
     if(dianji>=2000)if(flag[10]==1)flag[10]=0,document.getElementById("zzcsays2").innerHTML='获得成就 狂热LV.2：点击膜拜按钮2000次<br>单次点击膜拜数翻倍',orzDelta*=2,flag[0]=0,cjresttime=1,lastcj=obj.getHours()*3600+obj.getMinutes()*60+obj.getSeconds();
-    if(dianji>=6000)if(flag[11]==1)flag[11]=0,document.getElementById("zzcsays2").innerHTML='获得成就 狂热LV.3：点击膜拜按钮6000次<br>单次点击膜拜数翻倍',orzDelta*=2,flag[0]=0,cjresttime=1,lastcj=obj.getHours()*3600+obj.getMinutes()*60+obj.getSeconds();
+    if(dianji>=5000)if(flag[11]==1)flag[11]=0,document.getElementById("zzcsays2").innerHTML='获得成就 狂热LV.3：点击膜拜按钮5000次<br>单次点击膜拜数翻倍',orzDelta*=2,flag[0]=0,cjresttime=1,lastcj=obj.getHours()*3600+obj.getMinutes()*60+obj.getSeconds();
     tttt=obj.getHours()*3600+obj.getMinutes()*60+obj.getSeconds()-lastmb;
     if(tttt>=5)if(tttt<6)if(flag[12]==1)flag[12]=0,document.getElementById("zzcsays2").innerHTML='获得成就 虔诚LV.1<br>单次点击膜拜数变为原来的3倍',orzDelta*=3,flag[0]=0,cjresttime=1,lastcj=obj.getHours()*3600+obj.getMinutes()*60+obj.getSeconds();
     if(tttt>=12)if(tttt<13)if(flag[12]==0)if(flag[13]==1)flag[13]=0,document.getElementById("zzcsays2").innerHTML='获得成就 虔诚LV.2<br>单次点击膜拜数变为原来的4倍',orzDelta*=4,flag[0]=0,cjresttime=1,lastcj=obj.getHours()*3600+obj.getMinutes()*60+obj.getSeconds();    
